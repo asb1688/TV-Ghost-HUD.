108f1b47a4470d9daf19f9d27758b0b3411980f0
asb1688 <allison.s.beck@gmail.com>
2026-05-05 20:16:39 -0400

    Update ghost.js

--- ghost.js
+++ ghost.js
@@ -1,10 +1,1 @@
-(function () {
-  'use strict';
 
-  setTimeout(function() {
-    const sidebar = document.querySelector('.layout__area--right');
-    if (!sidebar) return;
-    sidebar.style.opacity = '0.2';
-  }, 5000);
-
-})();
